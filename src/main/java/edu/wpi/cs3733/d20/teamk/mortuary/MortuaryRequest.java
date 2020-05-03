301becc7ece75edb5432b40b03e9c515a08c3c20
trbouwens <42808108+trbouwens@users.noreply.github.com>
2020-05-03 15:49:44 -0700

    Finished Database implementation, Modified classes to fit data better, Tested everything.

--- src/main/java/edu/wpi/cs3733/d20/teamk/mortuary/MortuaryRequest.java
+++ src/main/java/edu/wpi/cs3733/d20/teamk/mortuary/MortuaryRequest.java
@@ -1,5 +1,9 @@
 package edu.wpi.cs3733.d20.teamk.mortuary;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.Optional;
+import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -7,73 +11,66 @@ import lombok.Setter;
 import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 
-import java.time.LocalDateTime;
-import java.util.Optional;
-import java.util.UUID;
-
 @AllArgsConstructor
 @Slf4j
 @EqualsAndHashCode
 @ToString
 public class MortuaryRequest {
-    @Getter @Setter
-    private UUID id;
+  @Getter @Setter private UUID id;
 
-    @Getter @Setter
-    private LocalDateTime openedTime;
-    private LocalDateTime closedTime;
+  @Getter @Setter private LocalDateTime openedTime;
+  private LocalDateTime closedTime;
 
-    @Getter @Setter
-    private Employee creator;
-    @Getter @Setter
-    private Person deceased;
+  @Getter @Setter private Employee creator;
+  @Getter @Setter private Person deceased;
 
-    @Getter @Setter
-    private Circumstance circumstance;
-    @Getter @Setter
-    private LocalDateTime time;
-    @Getter @Setter
-    private String description;
-    @Getter @Setter
-    private String location;
+  @Getter @Setter private Circumstance circumstance;
+  @Getter @Setter private LocalDateTime time;
+  @Getter @Setter private String description;
+  @Getter @Setter private String location;
 
-    public MortuaryRequest(Employee creator, Person deceased, Circumstance circumstance, LocalDateTime time, String description, String location) {
-        this.id = UUID.randomUUID();
-        this.openedTime = LocalDateTime.now();
-        this.closedTime = null;
+  public MortuaryRequest(
+      Employee creator,
+      Person deceased,
+      Circumstance circumstance,
+      LocalDateTime time,
+      String description,
+      String location) {
+    this.id = UUID.randomUUID();
+    this.openedTime = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS);
+    this.closedTime = null;
 
-        this.creator = creator;
-        this.deceased = deceased;
-        this.circumstance = circumstance;
-        this.time = time;
-        this.description = description;
-        this.location = location;
-    }
+    this.creator = creator;
+    this.deceased = deceased;
+    this.circumstance = circumstance;
+    this.time = time.truncatedTo(ChronoUnit.SECONDS);
+    this.description = description;
+    this.location = location;
+  }
 
-    public boolean isClosed() {
-        return this.closedTime != null;
-    }
+  public boolean isClosed() {
+    return this.closedTime != null;
+  }
 
-    public boolean isOpen() {
-        return this.closedTime == null;
-    }
+  public boolean isOpen() {
+    return this.closedTime == null;
+  }
 
-    public void close() {
-        this.closedTime = LocalDateTime.now();
-    }
+  public void close() {
+    this.closedTime = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS);
+  }
 
-    public void reopen() {
-        this.closedTime = null;
-    }
+  public void reopen() {
+    this.closedTime = null;
+  }
 
-    public Optional<LocalDateTime> getClosedTime() {
-        return Optional.of(this.closedTime);
-    }
+  public Optional<LocalDateTime> getClosedTime() {
+    return Optional.ofNullable(this.closedTime);
+  }
 
-    public void printCertificate() {
-        log.info("Printing death certificate for: " + this.deceased);
-        log.info("Request: " + this);
-        //TODO print something
-    }
-
+  public void printCertificate() {
+    log.info("Printing death certificate for: " + this.deceased);
+    log.info("Request: " + this);
+    // TODO print something
+  }
 }
--- src/main/java/edu/wpi/cs3733/d20/teamk/mortuary/Person.java
+++ src/main/java/edu/wpi/cs3733/d20/teamk/mortuary/Person.java
@@ -1,18 +1,18 @@
 package edu.wpi.cs3733.d20.teamk.mortuary;
 
+import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class Person {
-    private String id;
-    private String name;
-    private String gender;
-    private int age;
+  private UUID id;
+  private String name;
+  private String gender;
+  private int age;
 
-    public Person(String name, String gender, int age) {
-        this(name, name, gender, age);
-    }
-
+  public Person(String name, String gender, int age) {
+    this(UUID.randomUUID(), name, gender, age);
+  }
 }
