5936ab3a5d3ffd4cd8dcb3029b59a70fbd47f6c0
Christian M. Schrader <cmschrader@wpi.edu>
2020-05-04 00:22:25 -0400

    More Progress

--- src/main/java/edu/wpi/cs3733/d20/teamk/mortuary/impl/views/NewRequestController.java
+++ src/main/java/edu/wpi/cs3733/d20/teamk/mortuary/impl/views/NewRequestController.java
@@ -3,11 +3,38 @@ package edu.wpi.cs3733.d20.teamk.mortuary.impl.views;
 import io.github.socraticphoenix.jamfx.JamController;
 import io.github.socraticphoenix.jamfx.JamEnvironment;
 import io.github.socraticphoenix.jamfx.JamProperties;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.util.Pair;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class NewRequestController extends JamController {
+  @FXML Pane fieldPane;
+  RequestFieldsController fields;
 
   public NewRequestController(JamEnvironment environment, JamProperties properties, Scene scene) {
     super(environment, properties, scene);
+  }
+
+  @Override
+  public void init() {
+    super.init();
+    this.getScene().getStylesheets().add(getClass().getResource("default.css").toExternalForm());
+    Pair<RequestFieldsController, Pane> pair =
+        JamController.load(
+            RequestFieldsController.class.getResource("requestFields.fxml"),
+            this.getScene(),
+            this.getEnvironment(),
+            this.makeChildProperties());
+    this.fields = pair.getKey();
+    fieldPane.getChildren().add(pair.getValue());
+  }
+
+  @FXML
+  public void submit(ActionEvent actionEvent) {
+    this.fields.setEdit(false);
   }
 }
--- src/main/java/edu/wpi/cs3733/d20/teamk/mortuary/impl/views/RequestFieldsController.java
+++ src/main/java/edu/wpi/cs3733/d20/teamk/mortuary/impl/views/RequestFieldsController.java
@@ -1,15 +1,43 @@
 package edu.wpi.cs3733.d20.teamk.mortuary.impl.views;
 
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextArea;
+import com.jfoenix.controls.JFXTextField;
 import io.github.socraticphoenix.jamfx.JamController;
 import io.github.socraticphoenix.jamfx.JamEnvironment;
 import io.github.socraticphoenix.jamfx.JamProperties;
+import javafx.fxml.FXML;
 import javafx.scene.Scene;
 
 public class RequestFieldsController extends JamController {
+  @FXML private JFXTextField nameOfDeceased;
+  @FXML private JFXTextField gender;
+  @FXML private JFXComboBox circumstance;
+  @FXML private JFXTextArea description;
+  @FXML private JFXTextField timeOfDeath;
+  @FXML private JFXTextField dateOfDeath;
+  @FXML private JFXTextField place;
+
+
 
   public RequestFieldsController(
       JamEnvironment environment, JamProperties properties, Scene scene) {
     super(environment, properties, scene);
+  }
+
+  /**
+   * Sets whether or not the fields are user editable.
+   *
+   * @param editMode True for edit mode.
+   */
+  public void setEdit(boolean editMode) {
+    this.nameOfDeceased.setDisable(!editMode);
+    this.gender.setDisable(!editMode);
+    this.circumstance.setDisable(!editMode);
+    this.description.setDisable(!editMode);
+    this.timeOfDeath.setDisable(!editMode);
+    this.dateOfDeath.setDisable(!editMode);
+    this.place.setDisable(!editMode);
   }
 
   @Override
