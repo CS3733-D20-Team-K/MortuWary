0b0d6d0c42c3623049ff2330d38d0ff5a5117de1
trbouwens <42808108+trbouwens@users.noreply.github.com>
2020-05-11 16:17:06 -0700

    Meme Police

--- src/main/java/edu/wpi/cs3733/d20/teamk/mortuary/Circumstance.java
+++ src/main/java/edu/wpi/cs3733/d20/teamk/mortuary/Circumstance.java
@@ -2,65 +2,30 @@ package edu.wpi.cs3733.d20.teamk.mortuary;
 
 /**
  * Enumeration of death circumstance.
- *
- * <li>{@link #NATURAL}</li>
- * <li>{@link #DISEASE}</li>
- * <li>{@link #ACCIDENT}</li>
- * <li>{@link #COMPLICATIONS}</li>
- * <li>{@link #SUICIDE}</li>
- * <li>{@link #CRIME}</li>
- * <li>{@link #OTHER}</li>
- * <li>{@link #PENDING}</li>
- * <li>{@link #CS3733}</li>
- * <li>{@link #DAKA}</li>
+ * <li>{@link #NATURAL}
+ * <li>{@link #DISEASE}
+ * <li>{@link #ACCIDENT}
+ * <li>{@link #COMPLICATIONS}
+ * <li>{@link #SUICIDE}
+ * <li>{@link #CRIME}
+ * <li>{@link #OTHER}
+ * <li>{@link #PENDING}
  */
 public enum Circumstance {
-  /**
-   * Death by natural causes, old age or otherwise.
-   */
+  /** Death by natural causes, old age or otherwise. */
   NATURAL,
-  /**
-   * Death by disease, virus, sickness, or similar
-   * conditions.
-   */
+  /** Death by disease, virus, sickness, or similar conditions. */
   DISEASE,
-  /**
-   * Death by accidental injuries such as car or
-   * workplace accidents.
-   */
+  /** Death by accidental injuries such as car or workplace accidents. */
   ACCIDENT,
-  /**
-   * Death due to medical complication from surgery
-   * or other treatments.
-   */
+  /** Death due to medical complication from surgery or other treatments. */
   COMPLICATIONS,
-  /**
-   * Death purposefully caused the person themselves,
-   * usually due to mental illness.
-   */
+  /** Death purposefully caused the person themselves, usually due to mental illness. */
   SUICIDE,
-  /**
-   * Death by violent crime such as gun violence,
-   * terrorism, or other forms of murder.
-   */
+  /** Death by violent crime such as gun violence, terrorism, or other forms of murder. */
   CRIME,
-  /**
-   * Death that does not fit into other categories.
-   */
+  /** Death that does not fit into other categories. */
   OTHER,
-  /**
-   * Death circumstances unknown or awaiting
-   * determination.
-   */
-  PENDING,
-  /**
-   * Death related to or caused by software
-   * engineering.
-   */
-  CS3733,
-  /**
-   * Death related to or caused by Chartwell's
-   * Pulse on Dining colloquially known as DAKA.
-   */
-  DAKA
+  /** Death circumstances unknown or awaiting determination. */
+  PENDING
 }
